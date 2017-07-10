1df3cbe31a015696ad28ba906eb2e988d30ab15e
minu youn <anomie7777@gmail.com>
2017-07-10 21:48:44 +0900

    MySingleLinkedList] 20170710 연결리스트 구현 중

--- MySingleLinkedList/src/MySingleLinkedLS/MySingleLinkedList.java
+++ MySingleLinkedList/src/MySingleLinkedLS/MySingleLinkedList.java
@@ -7,13 +7,96 @@ public class MySingleLinkedList<T> {
 	
 	public void firstAdd(T item){
 		Node<T> tmp = new Node<T>(item);
+		tmp.next = head;
 		head = tmp;
 		size++;
 	}
 	
+	public void addAfter(Node<T> before, T item){
+		Node<T> tmp = new Node<T>(item);
+		tmp.next = before.next;
+		before.next = tmp;
+		size++;
+	}
+	
+	public void addLast(T item){
+		Node<T> tmp = new Node<T>(item);
+		Node<T> p = head;
+		while(p.next != null){
+			p = p.next;
+		}
+		p.next = tmp;
+	}
+	
+	public void removeFirst(){
+		if(head == null){
+			return;
+		}
+		head = head.next;
+		size--;
+	}
+	
+	public void removeAfter(Node<T> before){
+		if(before.next == null){
+			return;
+		}
+		
+		before.next = before.next.next;
+		size--;
+	}
+	
+	public int IndexOf(T item){
+		int idx = 0;
+		Node<T> tmp = head;
+		
+		while(tmp != null){
+			if(tmp.data.equals(item)){
+				return idx;
+			}
+			tmp = tmp.next;
+			idx++;
+		}
+		return -1;
+	}
+	
+	public Node<T> getNode(int idx){
+		if(idx < 0 || idx >= size)
+			return null;
+		
+		Node<T> tmp = head;
+		for(int i = 0; i < idx; i++){
+			tmp = tmp.next;
+		}
+		
+		return tmp;
+	}
+  	
+	public T get(int idx){
+		if(idx < 0 || idx >= size)
+			return null;
+		
+		Node<T> tmp = head;
+		for(int i = 0; i < idx; i++){
+			tmp = tmp.next;
+		}
+		
+		return tmp.data;
+	}
+	
+	public void add(int idx, T item){
+		if(idx < 0 || idx >= size){
+			return;			
+		}else if(idx == 0){
+			firstAdd(item);
+		}else{
+			Node<T> before = getNode(idx-1);
+			addAfter(before, item);
+		}
+	}
+	
 	public static void main(String[] args){
 		MySingleLinkedList<String> test = new MySingleLinkedList<>();
-		test.firstAdd("»ç¶ûÇØ");
+		test.firstAdd("");
 		
 	}
 
