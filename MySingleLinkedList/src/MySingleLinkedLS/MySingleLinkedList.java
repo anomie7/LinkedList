196e4d754452aa47a82b606dbf08b4a6c2e883be
minu youn <anomie7777@gmail.com>
2017-07-11 09:07:16 +0900

    MySingleLinkedList] 20170711 am 9:07 연결리스트 구현 완료(테스트 해야함)

--- MySingleLinkedList/src/MySingleLinkedLS/MySingleLinkedList.java
+++ MySingleLinkedList/src/MySingleLinkedLS/MySingleLinkedList.java
@@ -3,54 +3,56 @@ package MySingleLinkedLS;
 public class MySingleLinkedList<T> {
 	public Node<T> head = null;
 	public int size = 0;
-	
-	
-	public void firstAdd(T item){
+
+	public void firstAdd(T item) {
 		Node<T> tmp = new Node<T>(item);
 		tmp.next = head;
 		head = tmp;
 		size++;
 	}
-	
-	public void addAfter(Node<T> before, T item){
+
+	public void addAfter(Node<T> before, T item) {
 		Node<T> tmp = new Node<T>(item);
 		tmp.next = before.next;
 		before.next = tmp;
 		size++;
 	}
-	
-	public void addLast(T item){
+
+	public void addLast(T item) {
 		Node<T> tmp = new Node<T>(item);
 		Node<T> p = head;
-		while(p.next != null){
+		while (p.next != null) {
 			p = p.next;
 		}
 		p.next = tmp;
 	}
-	
-	public void removeFirst(){
-		if(head == null){
-			return;
+
+	public T removeFirst() {
+		if (head == null) {
+			return null;
 		}
+		Node<T> tmp = head;
 		head = head.next;
 		size--;
+		return tmp.data;
 	}
-	
-	public void removeAfter(Node<T> before){
-		if(before.next == null){
-			return;
+
+	public T removeAfter(Node<T> before) {
+		Node<T> tmp = before.next;
+		if (tmp == null) {
+			return null;
 		}
-		
-		before.next = before.next.next;
+		before.next = tmp.next;
 		size--;
+		return tmp.data;
 	}
-	
-	public int IndexOf(T item){
+
+	public int IndexOf(T item) {
 		int idx = 0;
 		Node<T> tmp = head;
-		
-		while(tmp != null){
-			if(tmp.data.equals(item)){
+
+		while (tmp != null) {
+			if (tmp.data.equals(item)) {
 				return idx;
 			}
 			tmp = tmp.next;
@@ -58,46 +60,54 @@ public class MySingleLinkedList<T> {
 		}
 		return -1;
 	}
-	
-	public Node<T> getNode(int idx){
-		if(idx < 0 || idx >= size)
+
+	public Node<T> getNode(int idx) {
+		if (idx < 0 || idx >= size)
 			return null;
-		
+
 		Node<T> tmp = head;
-		for(int i = 0; i < idx; i++){
+		for (int i = 0; i < idx; i++) {
 			tmp = tmp.next;
 		}
-		
+
 		return tmp;
 	}
-  	
-	public T get(int idx){
-		if(idx < 0 || idx >= size)
+
+	public T get(int idx) {
+		if (idx < 0 || idx >= size)
 			return null;
-		
+
 		Node<T> tmp = head;
-		for(int i = 0; i < idx; i++){
+		for (int i = 0; i < idx; i++) {
 			tmp = tmp.next;
 		}
-		
+
 		return tmp.data;
 	}
-	
-	public void add(int idx, T item){
-		if(idx < 0 || idx >= size){
-			return;			
-		}else if(idx == 0){
+
+	public void add(int idx, T item) {
+		if (idx < 0 || idx >= size) {
+			return;
+		} else if (idx == 0) {
 			firstAdd(item);
-		}else{
-			Node<T> before = getNode(idx-1);
+		} else {
+			Node<T> before = getNode(idx - 1);
 			addAfter(before, item);
 		}
 	}
-	
-	public static void main(String[] args){
-		MySingleLinkedList<String> test = new MySingleLinkedList<>();
-		test.firstAdd("");
-		
+
+	public T remove(int idx) {
+		if (idx < 0 || idx >= size) {
+			return null;
+		} else if (idx == 0) {
+			removeFirst();
+		}
+		Node<T> before = getNode(idx - 1);
+		return removeAfter(before);
 	}
 
+	public T remove(T item) {
+		int idx = IndexOf(item);
+		return remove(idx);
+	}
 }
